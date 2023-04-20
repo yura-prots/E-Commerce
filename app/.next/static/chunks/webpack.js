80d630b990c3f9b8513574c8a385698771305092
yura-prots <yuraprots@gmail.com>
2023-04-20 08:33:44 +0300

    sanity installed

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93909c532cbbfd7f"; }
+/******/ 		__webpack_require__.h = function() { return "5df78d7657bf9e9e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/.next/static/webpack/webpack.0a484e7e0ea1f73b.hot-update.js
+++ app/.next/static/webpack/webpack.0a484e7e0ea1f73b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "93909c532cbbfd7f"; }
+/******/ 	__webpack_require__.h = function() { return "ba741092a7e920ce"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
