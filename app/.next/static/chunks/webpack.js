7e9fa480221fd79f12161966641f4fb58ab3c053
yura-prots <yuraprots@gmail.com>
2023-04-21 09:37:45 +0300

    sanity client created

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35c3cfbd6434c53e"; }
+/******/ 		__webpack_require__.h = function() { return "fc0b2bc71bb531fd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
