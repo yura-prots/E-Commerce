505d67e512a5b433e9619079c5e9fb88efa3e9bc
yura-prots <yuraprots@gmail.com>
2023-04-21 09:19:31 +0300

    add HeroBanner

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d81b81d01bfae522"; }
+/******/ 		__webpack_require__.h = function() { return "35c3cfbd6434c53e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
