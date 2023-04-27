34e91b089f9d3d6433c228bd018e5126e47ccf42
yura-prots <yuraprots@gmail.com>
2023-04-27 14:42:59 +0300

    add totalPrice to Cart

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8d5897d4f45801f0"; }
+/******/ 		__webpack_require__.h = function() { return "030d11a6c82a1ca5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
